4bd9ccb6eb329f638913b1cbdc0b17208c13536e
Will <weiyuchens@sina.com>
2023-03-06 23:37:51 +0800

    first commit

---
+++ dataset.py
@@ -0,0 +1,10 @@
+class Dataset:
+
+    def __init__(self):
+        pass
+
+    def get_menu(self):
+        pass
+
+    def get_history(self):
+        pass
---
+++ recommend.py
@@ -0,0 +1,7 @@
+class Recommendation:
+
+    def __init__(self):
+        pass
+
+    def rec_by_random(self):
+        return
